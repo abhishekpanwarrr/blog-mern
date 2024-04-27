8d9910fc592e5898eee8238f79ec6934a8974e41
Abhishek Panwar <abhishekpanwar1718@gmail.com>
2024-04-27 19:43:35 +0530

    added signup

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,6 +9,7 @@ const Header = () => {
         <Navbar>
             <Link to={"/"} className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
                 <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">Abhishek's</span>
+                Blog
             </Link>
             <form>
                 <TextInput type="text" placeholder="Search..."
@@ -22,7 +23,7 @@ const Header = () => {
             <div className="flex gap-2 md:order-2">
                 <Button className="w-12 h-12 hidden sm:inline" color={"gray"} pill><FaMoon /></Button>
                 <Link to={"/sign-in"}>
-                    <Button className="" gradientDuoTone={"purpleToBlue"} pill>
+                    <Button className="" outline gradientDuoTone={"purpleToBlue"} pill>
                         Sign In
                     </Button>
                 </Link>
--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import { Button, Label, TextInput } from "flowbite-react"
+import { Link } from "react-router-dom"
 
 const SignUp = () => {
     return (
-        <div>SignUp</div>
+        <div className="min-h-screen mt-20">
+            <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+                {/* Left side */}
+                <div className="flex-1">
+                    <Link to={"/"} className="text-4xl font-bold dark:text-white">
+                        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">Abhishek's</span>Blog
+                    </Link>
+                    <p className="text-sm mt-5">
+                        This is a demo project you can signup with your email and password of with google
+                    </p>
+                </div>
+                {/* Right side */}
+                <div className="flex-1">
+                    <form className="flex flex-col gap-4">
+                        <div>
+                            <Label value="Your fullname" />
+                            <TextInput type="text" placeholder="rakesh kumar" id="fullName" />
+                        </div>
+                        <div>
+                            <Label value="Your email" />
+                            <TextInput type="email" placeholder="name@company.com" id="email" />
+                        </div>
+                        <div>
+                            <Label value="Your password" />
+                            <TextInput type="password" placeholder="strong@password" id="password" />
+                        </div>
+                        <Button gradientDuoTone={"purpleToPink"} type="submit">
+                            Sign up
+                        </Button>
+                        <div className="flex gap-2 text-sm mt-3">
+                            <span>Have an account?</span>
+                            <Link to={"/sign-in"} className="text-blue-500">
+                                Sign In
+                            </Link>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
