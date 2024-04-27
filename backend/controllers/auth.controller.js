e7a86e1fa9d861164509dad5c0f0df5d71a00ac8
Abhishek Panwar <abhishekpanwar1718@gmail.com>
2024-04-27 19:43:52 +0530

    added backend and singup code

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,92 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const createUser = async (req, res) => {
+  try {
+    const { fullName, email, password } = req.body;
+    if (
+      !fullName ||
+      !email ||
+      !password ||
+      fullName === "" ||
+      email === "" ||
+      password === ""
+    ) {
+      return res.status(400).json({ msg: "All fields are required!" });
+    }
+    let user = await User.findOne({ email: email });
+    if (user) {
+      return res
+        .status(409)
+        .json({ msg: `Email ${email} is already registered.` });
+    }
+    // Creating a new user and hashing the password using bcrypt
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email: email.toLowerCase(),
+      password: hashedPassword,
+    });
+    await newUser.save();
+    delete newUser.password;
+
+    return res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+// const loginUser = async (req, res) => {
+//   const { email, password } = req.body;
+//   if (!email || !password) {
+//     return res.status(400).json({ msg: "Please enter all details" });
+//   }
+//   try {
+//     const user = await User.findOne(email);
+//     if (!user) {
+//       return res.status(401).json({ msg: "Invalid Email or Password" });
+//     }
+//     const validPass = await bcrypt.compare(password, user.password);
+//     if (!validPass) {
+//       return res.status(401).json({ msg: "Invalid Email or Password" });
+//     }
+//     const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+//       expiresIn: "7d",
+//     });
+//     res.header("auth-token", token).send({ user, token });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).send().json({ msg: "Server Error" });
+//   }
+// };
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ msg: "Please enter all details" });
+  }
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ msg: "Invalid Email or Password" });
+    }
+    const validPass = await bcrypt.compare(password, user.password);
+    if (!validPass) {
+      return res.status(401).json({ msg: "Invalid Email or Password" });
+    }
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    // Create a new object without the password property
+    const userWithoutPassword = { ...user._doc };
+    delete userWithoutPassword.password;
+    res.cookie("auth-token", token, { maxAge: 7 * 24 * 60 * 60 * 1000 }); // Expires in 7 days
+    res.json({ user: userWithoutPassword, token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Server Error" });
+  }
+};
+
+export { createUser, loginUser };
---
+++ backend/index.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
+// Load environment variables from .env file
+dotenv.config();
+const app = express();
+
+// middlewares
+app.use(express.json({ extended: false }));
+app.use(cors());
+app.use(cookieParser());
+// ROUTES
+app.get("/", async (req, res) => {
+  res.send("Hello world");
+});
+// app.use("/api/v1/payment", paymentRouter);
+// app.use("/api/v1/product", productRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/auth", authRouter);
+
+// MONGOOSE SETUP
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection FAILED ", error);
+    process.exit(1);
+  }
+};
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 9000, () => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
